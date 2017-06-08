fb84e400fff105cc5e68aaf13ce14b09c284cd42
John Allan <johnjamesharvey@gmail.com>
2017-06-08 21:26:09 +0100

    filling out odels

--- models/artist.rb
+++ models/artist.rb
@@ -18,4 +18,21 @@ attr_accessor :id, :name
               @id = result[0]['id'].to_i
     end
 
+    def self.all()
+      sql = "SELECT * FROM artist"
+      results = SqlRunner.run( sql )
+      return results.map { |artist| Artist.new( artist ) }
+    end
+
+    def self.find( id )
+      sql = "SELECT * FROM album WHERE id=#{id}"
+      results = SqlRunner.run( sql )
+      return Artist.new( results.first )
+    end
+
+    def self.delete_all
+      sql = "DELETE FROM artist"
+      SqlRunner.run( sql )
+    end
+
 end
