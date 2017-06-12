9c3dd011266938d5ee837364d8f0df5c068f9dde
John Allan <johnjamesharvey@gmail.com>
2017-06-12 14:53:54 +0100

    finished MVP

--- controllers/store_controller.rb
+++
@@ -1,9 +0,0 @@
-# require( 'sinatra' )
-# require( 'sinatra/contrib/all' )
-# require_relative( '../models/store' )
-
-
-# get '/store/index' do
-#   @store = Store.all()
-#     erb(:"store/index")
-#   end
